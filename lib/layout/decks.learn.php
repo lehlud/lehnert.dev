6a0b7ceb806ee939aabd419efe6f04c94b54db25
Ludwig Lehnert <58045323+lehlud@users.noreply.github.com>
2025-11-09 11:05:51 +0100

    improved decks

--- lib/layout/decks.learn.php
+++ lib/layout/decks.learn.php
@@ -35,13 +35,13 @@ $card_ids = $deck->getCardIds();
 
         #back {
             padding: 0.3em 1.3em 0.3em 1.3em;
-            margin-top: 1.5em;
+            margin-top: 0.5em;
         }
 
-        #reveal, #next {
-            margin-top: 1em;
-            padding: 0.5em;
-            font-size: 1.2em;
+        button {
+            padding: 0.2em;
+            font-size: 1em;
+            margin: 0.35em;
         }
     </style>
 </head>
@@ -51,30 +51,74 @@ $card_ids = $deck->getCardIds();
 
     <div>
         <div id="front"></div>
-        <button id="reveal">Aufdecken</button>
         <div id="back"></div>
-        <button id="next">Weiter</button>
+
+        <button id="reveal">Aufdecken</button>
+
+        <div id="next-buttons">
+            <button id="false-answer">[q] Falsch</button>
+            <button id="true-answer">[w] Gewusst</button>
+        </div>
     </div>
 
     <script>
         const deckId = <?= json_encode($deck->id) ?>;
         const cardIds = <?= json_encode($card_ids) ?>;
 
+        let cardScores = JSON.parse(localStorage['_cardScores'] || '{}');
+        if (typeof cardScores !== 'object') cardScores = {};
+        const saveScores = () => localStorage['_cardScores'] = JSON.stringify(cardScores);
+
         const frontEl = document.getElementById('front');
         const revealEl = document.getElementById('reveal');
         const backEl = document.getElementById('back');
-        const nextEl = document.getElementById('next');
+        const nextButtonsEl = document.getElementById('next-buttons');
+        const falseAnswerEl = document.getElementById('false-answer');
+        const trueAnswerEl = document.getElementById('true-answer');
 
-        function hide(el) {
-            el.style.display = 'none';
+        const hide = (el) => el.style.display = 'none';
+        const unhide = (el) => el.style.display = 'block';
+        const hidden = (el) => el.style.display === 'none';
+
+        // init card scores
+        cardIds.forEach(id => cardScores[`${deckId}::${id}`] ||= 0);
+        saveScores();
+
+        const cardScore = (id) => cardScores[`${deckId}::${id}`] ?? 0;
+        const recordFalse = (id) => cardScores[`${deckId}::${id}`] /= 4;
+        const recordTrue = (id) => {
+            const prevScore = cardScores[`${deckId}::${id}`];
+            cardScores[`${deckId}::${id}`] = Math.min(31/32, prevScore + Math.pow(1 - prevScore, 2))
+        };
+
+        function getProbabilities() {
+            const invScores = {...cardScores};
+            Object.keys(invScores).forEach(key => {
+                if (!key.startsWith(`${deckId}::`)) {
+                    delete invScores[key];
+                    return;
+                }
+
+                invScores[key] = Math.pow(1 - invScores[key], 3);
+            });
+
+            const totalScore = Object.values(invScores).reduce((x, y) => x + y, 0);
+            return Object.fromEntries(
+                Object.entries(invScores).map(([key, value]) => [key, value / totalScore]),
+            );
         }
 
-        function unhide(el) {
-            el.style.display = 'block';
-        }
+        function pickNextCardId() {
+            const probabilities = getProbabilities();
 
-        async function getNextId() {
-            return cardIds[Math.floor(Math.random() * cardIds.length)];
+            const rand = Math.random();
+            let cumulative = 0;
+            for (const [key, prob] of Object.entries(probabilities)) {
+                cumulative += prob;
+                if (rand < cumulative) return key.split('::').at(-1);
+            }
+
+            throw new Error('failed to pick card id');
         }
 
         async function getCard(id) {
@@ -85,14 +129,15 @@ $card_ids = $deck->getCardIds();
             return { front, back };
         }
 
+        let currentCardId;
         async function next() {
             hide(frontEl);
             hide(revealEl);
             hide(backEl);
-            hide(nextEl);
+            hide(nextButtonsEl);
 
-            const nextId = await getNextId();
-            const nextCard = await getCard(nextId);
+            currentCardId = pickNextCardId();
+            const nextCard = await getCard(currentCardId);
 
             frontEl.innerHTML = nextCard.front;
             backEl.innerHTML = nextCard.back;
@@ -101,28 +146,50 @@ $card_ids = $deck->getCardIds();
             unhide(revealEl);
         }
 
-        function reveal() {
+        revealEl.addEventListener('click', () => {
             hide(revealEl);
             unhide(backEl);
-            unhide(nextEl);
-        }
+            unhide(nextButtonsEl);
+        });
 
-        revealEl.addEventListener('click', reveal);
-        nextEl.addEventListener('click', next);
+        falseAnswerEl.addEventListener('click', () => {
+            hide(nextButtonsEl);
+
+            recordFalse(currentCardId);
+            saveScores();
+            next();
+        });
+
+        trueAnswerEl.addEventListener('click', () => {
+            hide(nextButtonsEl);
+
+            recordTrue(currentCardId);
+            saveScores();
+            next();
+        });
 
         document.addEventListener('keydown', (e) => {
             if (e.code === 'Space') {
                 e.preventDefault();
+                if (hidden(revealEl)) trueAnswerEl.click();
+                else revealEl.click();
+            }
 
-                if (revealEl.style.display === 'none') {
-                    next();
-                } else {
-                    reveal();
-                }
+            if (e.code === 'KeyQ') {
+                e.preventDefault();
+                if (hidden(revealEl)) falseAnswerEl.click();
+            }
+
+            if (e.code === 'KeyW') {
+                e.preventDefault();
+                if (hidden(revealEl)) trueAnswerEl.click();
             }
         });
 
         next();
+
+        window.logScores = () => console.log(cardScores);
+        window.logProbabilities = () => console.log(getProbabilities());
     </script>
 </body>
 </html>
